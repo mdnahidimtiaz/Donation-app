1a7189ea64c27a4b23493185359e7bac35f40e34
mdnahidimtiaz <md.nah.imtiaz@gmail.com>
2024-10-27 21:33:44 +0600

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,16 +6,13 @@ const About = () => {
     <div className="about">
       <h1>About Our Donation </h1>
       <p>
-      The foundation of our donation platform is the conviction that every gift, no matter how modest, has the power to significantly improve the lives of people who are less fortunate. Our goal is to match contributors with important causes.
+      People for people. We want to help people for various purposes. We also contribute to our society.
       </p>
-      
-      <p>
-      Openness is essential to our goal. We make sure that all donations are monitored and documented so that you are aware of the precise ways in which your money is being applied to effect positive change.
-      </p>
+
 
       <h1>Our Mission</h1>
       <p>
-      My goal in donating is to help people in need by giving them access to necessities and bringing social issues in my neighborhood to light. My goal is to encourage kindness and giving in order to make the world a better place for future generations.
+      We donate to poor people for education and food. We also donate to improve medical facilities.
       </p>
     </div>
   );
